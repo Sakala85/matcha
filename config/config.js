19f7510e7a80c2925694797bdb8a7dc2f3c912a4
akhoucha <35508550+akhoucha@users.noreply.github.com>
2020-03-18 19:06:17 +0100

    retour a la version initial avec commentaires

--- config/config.js
+++
@@ -1,4 +0,0 @@
-//Cette clé secrète sera utilisée lors de la génération de nos tokens JWT et permet d’instaurer une sécurité dans nos jetons d’authentifications.
-module.exports = {
-    "secret" : "mediumMatchaCreation"
-}
