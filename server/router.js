ee02fb8e654500aeeede6ed2be1e173d27c46d9b
melchior <melchiorbengtsson@gmail.com>
2020-03-19 03:58:12 +0100

    ok

--- server/router.js
+++ server/router.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
+  
 });
+// router.use("/user", user);
+//require(__dirname + "/controllers/userController")(router);
+
 
 module.exports = router;
